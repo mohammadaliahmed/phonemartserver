42f065ac8962c3c3b2b8540a203e8a4c2a1d685b
AliAh <m.aliahmed0@gmail.com>
2020-05-28 13:22:25 +0500

    images

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 <div id="app">
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="http://phonemart.co"> <img src="public/logo.png" width="50%">
+        <a class="navbar-brand" href="http://phonemart.co"> <img src="/public/logo.png" width="50%">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
